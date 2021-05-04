a5bb8993c08513e0d7168b0af09224537285ef12
Muhammad Irtiza <66883048+irtiza7@users.noreply.github.com>
2021-05-04 12:00:11 +0500

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,10 @@
+public class Game{
+    protected String player1;
+    protected String player2;
+
+    public void start(){}
+    public int stop(Value[][] table, int moveCounter){
+        return 0;
+    }
+    public void restart(){}
+}
