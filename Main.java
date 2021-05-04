55ad2b1877ee26c37a9dfc8890f6514a5039c55d
Muhammad Irtiza <66883048+irtiza7@users.noreply.github.com>
2021-05-04 12:38:53 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n................WELCOME TO TIC TAC TOE GAME................\n");
+
+        //creating object of tic tac tor class
+        TicTacToe game = new TicTacToe();
+
+        //  start() starts the game
+        game.start();
+
+    }
+}
